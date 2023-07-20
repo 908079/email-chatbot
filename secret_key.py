45aa61f76f7c0cbd5efada3503966007ac2bdcac
Miguel Ángel Hernández <miguelangel.hermar410@gmail.com>
2023-07-19 22:17:15 -0600

    Update secret_key.py

--- secret_key.py
+++ secret_key.py
@@ -1,16 +1,1 @@
-openapi_key = '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'
-serpapi_key = '7756d19f90ea6c11f4506e26759ec7bb1e04af5f61fdb31a9d93583983a38e78'
-pinecone_key = 'b51b53bf-8697-4a05-9bd8-b45094f9eb36'
-environment = 'us-west1-gcp-free'
 
-zapier_client_id = "nla-Eitle9g6OZhtyyIEsrdL7FMTzkwUQgBbuO5B"
-zapier_redirect_uri = 'https://mail.google.com/mail/u/0/'
-# zapier_redirect_uri = "https://680f-2806-2f0-9101-c1ba-e01b-97ec-1f78-e9a3.ngrok-free.app"
-zapier_client_secret = "862ddcb70155a4f858b75b4d748904dcbc34823c2d3b25af5de1f118f3dc7dc7"
-
-# authorization_endpoint = "https://nla.zapier.com/oauth/authorize/"
-# token_endpoint = "https://nla.zapier.com/oauth/token/"
-# redirect_uri = "https://mail.google.com/mail/u/0/"
-# client_id = "nla-Eitle9g6OZhtyyIEsrdL7FMTzkwUQgBbuO5B"
-# client_secret = "862ddcb70155a4f858b75b4d748904dcbc34823c2d3b25af5de1f118f3dc7dc7"
-# scope = "nla%3Aexposed_actions%3Aexecute"
