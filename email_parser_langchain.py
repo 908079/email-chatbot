0c8f8723961f59220bd860371051a5a469de6d0d
Miguel Ángel Hernández <miguelangel.hermar410@gmail.com>
2023-07-19 22:32:49 -0600

    Update email_parser_langchain.py

--- email_parser_langchain.py
+++ email_parser_langchain.py
@@ -22,8 +22,8 @@ from langchain.schema import (
 )
 
 zapier_client_id = st.secrets["zapier_client_id"]
-# zapier_redirect_uri = st.secrets["zapier_redirect_uri"]
-# zapier_client_secret = st.secrets["zapier_client_secret"]
+zapier_redirect_uri = st.secrets["zapier_redirect_uri"]
+zapier_client_secret = st.secrets["zapier_client_secret"]
 
 def init():
         
